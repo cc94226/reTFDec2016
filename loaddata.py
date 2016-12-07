0e72a1e834e79d8548af7508aa882164c5ead951
Cheng Chen <che313@bragg-gpu.cm.hpc>
2016-12-07 15:23:00 +1100

    no use

--- loaddata.py
+++ loaddata.py
@@ -1,7 +1,7 @@
-emb_path = /data/che313/reDec2016data/embedding/SKIP_GRAM_average_v5_200.th
+emb_path = "/data/che313/reDec2016data/embedding/SKIP_GRAM_average_v5_200.th"
 
-local emb = torch.load(emb_path)
-local emb_vecs = emb.M:double()
-local emb_vocab = emb.w2vvocab
+emb = torch.load(emb_path)
+emb_vecs = emb.M
+emb_vocab = emb.w2vvocab
 
 print "ok"
