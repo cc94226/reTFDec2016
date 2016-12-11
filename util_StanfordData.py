3e9372a1104f094d8ecf3150fb339097076ae9eb
cc94226 <cc94226@live.com>
2016-12-11 18:22:19 +1100

    on going 2

--- util_StanfordData.py
+++ util_StanfordData.py
@@ -48,7 +48,7 @@ def read_features(path,bottom_line,line_num):
 				a[i].append((int)tokens[i] + 1)
 				a[i].append(1)
 			#end for i
-		count++
+		count+= 1
 	#end while
 	
 	file.close()
@@ -76,7 +76,7 @@ def read_sentences(sentence_path,emb_vocab,bottom_line,line_num):
 				#end else
 			#end for i
 		sentences.append(snet)
-		count++
+		count+=1
 	#end while
 	
 	file.close()
@@ -99,7 +99,7 @@ def read_seq_ids(seq_path,bottom_line,line_num):
 					
 				seq_ids.append(ids)
 			#end if
-		count++
+		count+=1
 	#end while
 	
 	fiel.close()
@@ -119,3 +119,75 @@ def read_trees_from_seq_ids(seqs,window_size,sents,predict_root,filter_list,dire
 		trees.append(tree)
 	#end for i
 	return trees
+
+def read_tree(parents, seqs,window_size,sent,predict_root,filter_list,direction):
+	size = len(parents)
+	trees = []
+	
+	root_count = 0
+	for i in range(i, size):
+		#----------
+		if not trees[i]:
+			idx = i
+			prev = None
+			while True:
+				parent = parents[idx]
+				sent_idx = seqs[idx]
+				#~~~~~~~~~~~~~~~~~~~~
+				tree = treelstm.Tree()
+				
+				tree.is_root = False
+				if prev != None:
+					tree.add_child(prev)
+				#end if
+				
+				trees[idx] = tree
+				tree.sent_idx = sent_idx
+				tree.idx = idx
+				#~~~~~~~~~
+        			set_spans(tree)
+				
+				c = 0
+				for j = tree.lo - window_size, tree.hi + window_size:
+					if j >= 1 and j <= len(sent) and (j > tree.hi or j < tree.lo):
+						if filter_list ! = None:
+							if filter_list[sent[j][1]] == None:
+								c+=1
+								#~~~~
+								treelstm.addToSet(tree.context_word_id_set,sent[j][1])
+							#end if filter_list[]
+						else:
+							treelstm.addToSet(tree.context_word_id_set,sent[j][1])
+						#end else
+					#end if j
+				#end for
+				if trees[parent] != None:
+          				trees[parent].add_child(tree)
+          				break
+        			elif parent == size + 1:
+          				root = tree
+          				root_count = root_count + 1
+          				tree.is_root = True
+          				break
+        			else:
+          				prev = tree
+          				idx = parent
+				#end else
+			#end while
+		#end if not tree
+	#end for i
+	
+	if predict_root == 2:
+      		root.context_word_id_set = []
+		lo = 0
+		hi = 0
+      		if direction == 'l':
+			lo = root.lo + 1
+			hi = root.hi
+		elif direction == 'r':
+			lo = root.lo
+			hi = root.hi - 1
+		#end elif
+		
+		for j in range(lo, hi):
+			
