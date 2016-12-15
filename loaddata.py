3f48f730eef2fa43250f81f45b2d7a23d3352e5f
Cheng Chen <che313@bragg-gpu.cm.hpc>
2016-12-15 13:51:08 +1100

    fix

--- loaddata.py
+++ loaddata.py
@@ -1,6 +1,6 @@
 import util_StanfordData as utilSIE
 
-test_dir = '/data/che313/reTF2016/bipath_stanford/test'
+test_dir = '/data/che313/reTF2016/bipath_stanford/test/'
 max_line = 2240
 
 test_dataset = utilSIE.read_whole_stanfordIE_data(test_dir,1, max_line, 0)
