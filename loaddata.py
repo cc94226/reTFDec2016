7a816e000d8f67573f761df9716600f8d0dbfba5
Cheng Chen <che313@hydrogen-cc.nexus.csiro.au>
2016-12-15 13:48:40 +1100

    for test2

--- loaddata.py
+++ loaddata.py
@@ -1,4 +1,7 @@
 import util_StanfordData as utilSIE
 
-utilSIE.read_whole_stanfordIE_data()
+test_dir = '/data/che313/reTF2016/bipath_stanford/test'
+max_line = 2240
+
+test_dataset = utilSIE.read_whole_stanfordIE_data(test_dir,1, max_line, 0)
 
