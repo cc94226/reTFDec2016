d92498acd2eb009c96b0b7d9b69117be67213547
Cheng Chen <che313@hydrogen-cc.nexus.csiro.au>
2016-12-15 16:30:08 +1100

    for test3

--- loaddata.py
+++ loaddata.py
@@ -1,7 +1,10 @@
 import util_StanfordData as utilSIE
+import util_loadEmbedding as utilEMB
 
+emb_path = '/home/che313/data/embedding/SKIP_GRAM_200_v5_FIGER_Stanford_0d001_50iter.txt'
 test_dir = '/data/che313/reTF2016/bipath_stanford/test/'
 max_line = 10000
 
-test_dataset = utilSIE.read_whole_stanfordIE_data(test_dir,1, max_line, 0)
-
+#test_dataset = utilSIE.read_whole_stanfordIE_data(test_dir,1, max_line, 0)
+words_num, emb_size = utilEMB.load_embedding(emb_path)
+print words_num, emb_size
---
+++ util_loadEmbedding.py
@@ -0,0 +1,18 @@
+def load_embedding(emb_path):
+    file = open(emb_path)
+    words_num, emb_size = [int(x) for x in next(file).split()]
+
+    w2vovcab = {}
+    v2wvocab = {}
+
+    M = []
+    #for i in range(0, words_num):
+        temp_line = next(file).split()
+        temp_word = temp_line[0]
+        temp_embdata = temp_line[1:]
+        M.append(temp_embdata)
+        w2vovcab[temp_word] = i
+        v2wvocab[i] = temp_word
+
+    print temp_word, temp_embdata
+    return words_num, emb_size
