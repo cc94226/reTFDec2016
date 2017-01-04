9e19cda12a259ad4d29c9d550ec058b6713bf3e7
cc94226 <cc94226@live.com>
2017-01-04 12:04:45 +1100

    suspended on cnn model

--- CNN_Class.py
+++ CNN_Class.py
@@ -1,3 +1,5 @@
+import tensorflow as tf
+
 class CNN_Class:
     def __init__(self, config):
         self.mem_dim = config.mem_dim or 200
@@ -9,3 +11,23 @@ class CNN_Class:
         self.batch_size = config.batch_size or 100
         self.emb_vocab = config.emb_vocab
         self.emb_v2wvocab = config.emb_v2wvocab
+        self.left_pos_model = tf.nn.embedding.lookup(self.left_pos_window, self.pos_dim)
+        self.right_pos_model = tf.nn.embedding.lookup(self.right_pos_window, self.pos_dim)
+        self.emb_vecs = config.emb_vecs
+        self.combine = config.combine or False
+
+        if self.emb_vecs:
+            self.initLookupTable(self.emb_vecs, self.emb_vocab)
+
+        config.emb_vecs = None
+
+        self.structure = config.structure or 'pcnn'
+        #?
+        self.reg = config.reg or 1e-4
+        self.pcnn_window = 3
+        self.pcnn_step = 1
+        # join talbe
+        self.combine_feature_model =
+        # padding for cnn
+
+        #temporal convolution not support by tensorflow??
