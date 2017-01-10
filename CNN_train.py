0163136a5def9b934d037a020ecb8be5a072b0b3
cc94226 <cc94226@live.com>
2017-01-10 21:57:40 +1100

    solving data

---
+++ CNN_train.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+import numpy as np
+import os
+import time
+import datetime
+import data_helpers
+from CNN_Class import CNN_Class
+
+import util_StanfordData as utilSIE
+import util_loadEmbedding as utilEMB
+
